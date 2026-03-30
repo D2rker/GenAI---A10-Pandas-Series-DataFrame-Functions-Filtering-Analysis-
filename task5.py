f5d5e3d07b3184ee22993e9e63e321ec7811dff2
Vikas kimothi <108613154+D2rker@users.noreply.github.com>
2026-03-30 11:31:08 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,1 +1,29 @@
+import pandas as pd
 
+students_data = {
+    'Name' : ['amit', 'neha', 'rahul', 'sneha', 'pooja'],
+    'Marks' : [78, 85, 90, 66, 72],
+    'Subject' : ['math', 'math', 'science', 'science', 'math']
+}
+
+students = pd.DataFrame(students_data)
+
+# print information
+print("info")
+students.info()
+
+print("\n describe")
+print(students.describe())
+
+print("\n head")
+print(students.head())
+
+print("\n tail")
+print(students.tail())
+
+
+# sort students by marks in decending order
+students_sorted = students.sort_values(by='Marks', ascending=False).reset_index(drop=True)
+
+print("\n--- Sorted and Reset Index ---")
+print(students_sorted)
